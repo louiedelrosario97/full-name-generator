1650c1d3cff2c297603453feaf3ecfd90bc27efc
Louie D <louie.delrosario97@gmail.com>
2026-04-13 20:30:45 -0700

    Created scanner entries for first, middle, last name, suffix. Added .trim function to trim whitespace

--- src/com/pluralsight/FullNameApplication.java
+++ src/com/pluralsight/FullNameApplication.java
@@ -1,6 +1,49 @@
-package com.pluralsight;public class FullNameApplication {
+import java.util.Scanner;
+
+public class FullNameApplication {
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in); // Add Scanner
+
+        System.out.print ("Enter First Name: "); // Prompt first name entry
+        String firstName = scan.nextLine().trim(); // Scan for variable 'firstName'
+
+        System.out.print ("Enter Middle Name"); // Prompt middle name entry
+        String middleName = scan.nextLine().trim(); // Scan for variable 'middleName'
+
+        System.out.print ("Enter Last Name: "); // Prompt last name entry
+        String lastName = scan.nextLine().trim(); // Scan for variable 'lastName'
+
+        System.out.print ("Enter Suffix: "); // Prompt suffix entry
+        String suffix = scan.nextLine().trim(); // Scan for variable 'suffix'
+
+
+
+
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
